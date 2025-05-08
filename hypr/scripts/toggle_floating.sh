27c3fa31304b0b40c914969a49c712bc277d2208
Adrian <adrian.aires.c@gmail.com>
2025-05-08 14:53:47 -0300

    inserted toggle_floating.sh to solve size and position when toggling

---
+++ hypr/scripts/toggle_floating.sh
@@ -0,0 +1,18 @@
+# get the class and floating status of the active window
+active_class=$(hyprctl activewindow | awk -F": " '/class:/ {print $2}')
+is_floating=$(hyprctl activewindow | awk -F": " '/floating:/ {print $2}')
+
+# if the window is already floating, toggle it back to tiled
+if [[ $is_floating == "1" ]]; then
+  hyprctl dispatch togglefloating
+  exit 0
+fi
+
+# resize and float the active window based on its class
+if [[ $active_class == *"kitty"* ]]; then
+  hyprctl dispatch togglefloating
+  hyprctl dispatch resizeactive exact 50% 100%
+  hyprctl dispatch centerwindow
+else
+  hyprctl dispatch togglefloating
+fi
