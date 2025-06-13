aab649e2324fd4cf02c07e8e2f0649230977e115
Adrian <adrian.aires.c@gmail.com>
2025-06-13 17:42:39 -0300

    Transparent tokyonight theme set on colorscheme.lua

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -4,6 +4,13 @@ return {
     window = {
       position = "right",
     },
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
   },
   keys = {
     { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
