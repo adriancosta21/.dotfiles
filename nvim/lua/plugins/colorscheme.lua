ceb30620fbd74cad1624ff15584b7355672f2e00
Adrian <adrian.aires.c@gmail.com>
2025-06-13 23:25:53 -0300

    Added missing fields, no more error message

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,29 @@
-require("tokyonight").setup({
-  transparent = true,
-  styles = {
-    sidebars = "transparent",
-    floats = "transparent",
+return {
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        style = "storm",
+        light_style = "day",
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+        day_brightness = 0.3,
+        dim_inactive = false,
+        lualine_bold = false,
+        on_colors = function(colors) end,
+        on_highlights = function(highlights, colors)
+          highlights.NeoTreeNormal = { bg = "NONE" }
+          highlights.NeoTreeNormalNC = { bg = "NONE" }
+        end,
+        plugins = nil,
+        cache = false,
+      })
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+    priority = 1000,
   },
-})
+}
