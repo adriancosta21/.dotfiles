f83a6b39dd178d8eb2d7aa9131a7fb62138de753
Adrian <adrian.aires.c@gmail.com>
2025-07-28 16:33:06 -0300

    Changed some keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -13,11 +13,5 @@ map("v", "<C-c>", '"+y', { desc = "Copy to system clipboard" })
 -- Ctrl+V para colar no Normal Mode
 map("n", "<C-v>", '"+p', { desc = "Paste from system clipboard" })
 
--- Ctrl+V para colar no Insert Mode
-map("i", "<C-v>", "<C-r>+", { desc = "Paste from system clipboard (insert mode)" })
-
--- Ctrl+V para colar no Visual Mode (substituindo seleção)
-map("v", "<C-v>", '"+p', { desc = "Paste over selection from clipboard" })
-
 -- Ctrl+Tab navega para o buffer anterior (último usado)
 map("n", "<C-Tab>", "<cmd>b#<cr>", { desc = "Switch to last buffer" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,7 +4,6 @@
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 -- Cor dos números relativos (LineNr) = azul
 vim.cmd([[
   highlight LineNr guifg=#569CD6
