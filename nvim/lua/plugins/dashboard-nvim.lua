d584934af119401552725bc7674533a28a1f61f9
Adrian <adrian.aires.c@gmail.com>
2025-08-06 19:35:27 -0300

    dashboard looks better with image on left

--- nvim/lua/plugins/dashboard-nvim.lua
+++ nvim/lua/plugins/dashboard-nvim.lua
@@ -1,24 +1,48 @@
--- lazy.nvim
 return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
     dashboard = {
-      -- your dashboard configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
       sections = {
+        -- Coluna da esquerda: Imagem grande em ASCII
         {
+          row = 1,
+          pane = 1,
           section = "terminal",
-          cmd = "ascii-image-converter ~/Pictures/eva-cross.jpg -C -d 20,10",
-          height = 10,
+          cmd = "ascii-image-converter ~/Pictures/eva-cross.jpg -C -d 40,20",
+          height = 20,
           padding = 1,
-          indent = 20,
         },
-        { section = "keys", gap = 1, padding = 1 },
-        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+
+        -- Coluna da direita: atalhos e listas
         {
+          row = 1,
+          pane = 2,
+          section = "keys",
+          gap = 1,
+          padding = 1,
+        },
+        {
+          row = 1,
+          pane = 2,
+          icon = " ",
+          title = "Recent Files",
+          section = "recent_files",
+          indent = 2,
+          padding = 1,
+        },
+        {
+          row = 1,
+          pane = 2,
+          icon = " ",
+          title = "Projects",
+          section = "projects",
+          indent = 2,
+          padding = 1,
+        },
+        {
+          row = 1,
+          pane = 2,
           icon = " ",
           title = "Git Status",
           section = "terminal",
