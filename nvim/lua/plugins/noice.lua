6cb0235f4003397db32beda1ac80e006af7a8c5e
Adrian <adrian.aires.c@gmail.com>
2025-08-11 14:17:18 -0300

    Added noice.lua to center commands

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,64 @@
+-- lazy.nvim
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+        size = {
+          width = "auto",
+          height = "auto",
+        },
+        border = {
+          style = "rounded",
+          padding = { 1, 2 },
+        },
+        filter_options = {},
+        win_options = {
+          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = "auto",
+          col = "auto",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+      },
+    },
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = false,
+      lsp_doc_border = false,
+    },
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  },
+}
