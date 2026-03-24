da95574646c6725f59eb2986f74c6171f1438f6d
adriancosta21 <adrian.aires.c@gmail.com>
2026-03-23 23:55:05 -0300

    Now every folder under ~/vaults/ work with the plugin

--- nvim/lua/plugins/obsidian-nvim.lua
+++ nvim/lua/plugins/obsidian-nvim.lua
@@ -3,32 +3,28 @@ return {
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
 
     -- see below for full list of optional dependencies 👇
   },
-  opts = {
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/vaults/personal",
-      },
-      {
-        name = "work",
-        path = "~/vaults/work",
-      },
-    },
+  opts = function()
+    local workspaces = {}
+    local vaults_path = vim.fn.expand("~/vaults/")
+    local folders = vim.fn.glob(vaults_path .. "*/", false, true)
 
-    -- see below for full list of options 👇
-  },
+    for _, folder in ipairs(folders) do
+      local name = vim.fn.fnamemodify(folder, ":h:t")
+      table.insert(workspaces, {
+        name = name,
+        path = folder,
+      })
+    end
+
+    return {
+      workspaces = workspaces,
+      -- see below for full list of options 👇
+    }
+  end,
 }
