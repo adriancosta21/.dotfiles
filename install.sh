e801ea4d8be09b5cb03cba6f5cf5716ff8d65d01
adriancosta21 <adrian.aires.c@gmail.com>
2026-07-11 17:46:18 -0300

    :sparkles: feat: expand installer with system checks, logging and automatic stow

--- install.sh
+++ install.sh
@@ -1,45 +1,173 @@
 #!/usr/bin/env bash
 
+# === VARIABLES ===
+
+DOTFILES_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
+
 # Official pacman packages
-pacman_packages=(
+PACMAN_PKG=(
+  # Shell
   zsh
   kitty
+  neovim
   ttf-jetbrains-mono-nerd
   zoxide
   starship
   eza
-  github-cli
-  yay
+  # System
   niri
-  dms-shell
-  fastfetch
+  xwayland-satellite
+  dms-shell-niri
   cava
   playerctl
-  grim
-  slurp
+  xdg-desktop-portal
   wl-clipboard
+  # Audio
+  pipewire
+  wireplumber
+  pipewire-pulse
+  pipewire-alsa
+  alsa-utils
+  sof-firmware
+  # Bluetooth
+  bluez
+  bluez-utils
+  # Video
+  vlc
+  # Printers
   system-config-printer
   cups
   cups-filters
+  nss-mdns
   cups-pk-helper
   ghostscript
   gsfonts
-  thunar
+  # CLI Tools
+  bat
+  btop
+  fastfetch
+  fd
+  fzf
+  github-cli
+  tmux
+  # Internet connection
+  iwd
+  dhcpcd
+  # Apps
+  dolphin
+  okular
   obsidian
+)
+
+# Will be installed with yay AUR helper
+AUR_PKG=(
+  dsearch-bin
+  onlyoffice-bin
+  spotify
   zen-browser-bin
 )
 
-# AUR packages
-aur_packages=(
-  dsearch-bin
-  lsfg-vk-git
-  spotify
+# Symlinks that will be created using GNU stow
+STOW_PKG=(
+  zsh
+  starship
+  tmux
+  fastfetch
+  kitty
+  niri
+  danksearch
 )
 
-echo "Installing official pacman packages..."
-sudo pacman -S --needed "${pacman_packages[@]}"
+# === SETUP ===
 
-echo "Installing AUR packages..."
-yay -S --needed "${aur_packages[@]}"
+LOGFILE="$HOME/dotfiles.log"
 
-echo "Instalation complete!"
+# Cleans up (or create) LOGFILE
+: >$LOGFILE
+
+# Send every stderr to LOFILE
+exec 2>>"$LOGFILE"
+
+# === FUNCTIONS ===
+
+check_arch() {
+  if ! command -v pacman &>/dev/null; then
+    echo "This install script must be used inside arch-based systems"
+    exit 1
+  fi
+}
+check_internet() {
+  echo "Checking internet connection..."
+  if ! ping -c1 -W3 archlinux.org &>/dev/null; then
+    echo "No internet connection"
+    exit 1
+  fi
+
+}
+compile_yay() {
+  echo "yay not installed. Compiling yay from source..."
+  sudo pacman -S --needed --noconfirm base-devel git &>/dev/null
+
+  local tmpdir
+  tmpdir=$(mktemp -d)
+  if git clone https://aur.archlinux.org/yay.git "$tmpdir/yay" &&
+    (cd $tmpdir && makepkg -si --noconfirm); then
+    echo "Yay installed successfully"
+  else
+    echo "Error while compiling yay"
+    exit 1
+  fi
+  rm -rf $tmpdir
+}
+
+pacman_packages() {
+  echo "Installing official pacman packages..."
+  if sudo pacman -S --needed --noconfirm "${PACMAN_PKG[@]}"; then
+    echo "Pacman packages installed successfully"
+  else
+    echo "Error while downloading pacman packages"
+    exit 1
+  fi
+}
+
+aur_packages() {
+  echo "Installing AUR packages using yay helper..."
+  if ! command -v yay; then
+    compile_yay
+  fi
+  if yay -S --needed --noconfirm "${AUR_PKG[@]}"; then
+    echo "Instalation complete!"
+  else
+    echo "Error while downloading AUR packages"
+    exit 1
+  fi
+}
+stow_dotfiles() {
+  echo "Creating symlinks for .dotfiles"
+  for pkg in "${STOW_PKG[@]}"; do
+    if ! stow -R --target="$HOME" "$pkg"; then
+      echo "error while creating symlink with GNU stow for package: $pkg. Config file  probably already exists"
+    fi
+  done
+}
+systemd_setup() {
+  echo "Setting up systemd services..."
+  services=(bluetooth cups avahi-daemon)
+  if sudo systemctl enable --now "${services[@]}" && systemctl --user enable dsearch; then
+    echo "Systemd services enabled correctly"
+  else
+    echo "Error while setting up systemd services"
+    exit 1
+  fi
+}
+
+# === CODE EXECUTION ===
+
+check_arch
+check_internet
+pacman_packages
+aur_packages
+stow_dotfiles
+systemd_setup
+echo "Instalation complete! Check ~/dotfiles.log for warnings or errors"
+exit 0
