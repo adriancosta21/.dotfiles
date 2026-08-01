a59c001162f2a2da8ad9f333a6c6595a5b6d3ba6
adriancosta21 <adrian.aires.c@gmail.com>
2026-08-01 13:03:47 -0300

    feat(nvim): add dictionary to .md buffers

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,5 +1,8 @@
 return {
   "blink.cmp",
+  dependencies = {
+    "Kaiser-Yang/blink-cmp-dictionary",
+  },
   opts = {
     completion = {
       list = {
@@ -9,10 +12,24 @@ return {
         },
       },
     },
+    sources = {
+      per_filetype = {
+        markdown = { "lsp", "path", "snippets", "buffer", "dictionary" },
+      },
+      providers = {
+        dictionary = {
+          module = "blink-cmp-dictionary",
+          name = "Dict",
+          min_keyword_length = 3,
+          max_items = 8,
+          opts = {
+            dictionary_files = { "/usr/share/dict/words" }, -- can be installed with the words package
+          },
+        },
+      },
+    },
     keymap = {
-      preset = "none", -- desabilita todos os keybinds padrão
-
-      -- Aceita sugestão apenas com <Tab> (supertab) ou <C-f>
+      preset = "none",
       ["<Tab>"] = {
         function(cmp)
           if cmp.snippet_active() then
@@ -25,15 +42,11 @@ return {
         "fallback",
       },
       ["<C-f>"] = { "accept", "fallback" },
-
       ["<C-n>"] = { "select_next", "fallback" },
       ["<C-p>"] = { "select_prev", "fallback" },
       ["<Up>"] = { "select_prev", "fallback" },
       ["<Down>"] = { "select_next", "fallback" },
-
-      -- Desabilita Enter
       ["<CR>"] = {},
-      -- Outros exemplos de navegação/documentação (opcional)
       ["<C-b>"] = { "scroll_documentation_up", "fallback" },
       ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
     },
